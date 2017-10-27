0700fa88bff7800474cd93375760ffa2dc9866b2
Andy Tu <andytu@live.ca>
2017-10-27 12:15:29 -0700

    assert 0 < x < 13 as well in load

--- src/state.py
+++ src/state.py
@@ -4,8 +4,6 @@ keyboard = PyKeyboard()
 
 
 def save(slot_num):
-    # pattern = re.compile(r'^F\d{1,2}$')
-    # assert pattern.match(slot_key)
     assert 0 < slot_num < 13
     keyboard.press_keys([keyboard.shift_key, keyboard.function_keys[slot_num]])
     keyboard.release_key(keyboard.shift_key)
@@ -13,4 +11,5 @@ def save(slot_num):
 
 
 def load(slot_num):
+    assert 0 < slot_num < 13
     keyboard.tap_key(keyboard.function_keys[slot_num])
