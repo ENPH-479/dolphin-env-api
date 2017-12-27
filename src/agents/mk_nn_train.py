26a881c02d04ebacbe1e105005e891c3527ba3e5
andytuwm <andytu@live.ca>
2017-12-27 14:36:52 -0800

    use cuda if available for mknn

--- src/agents/mk_nn_train.py
+++ src/agents/mk_nn_train.py
@@ -49,9 +49,16 @@ class MKRNN(nn.Module):
             nn.Linear(self.hidden_size_3, self.output_vec)
         )
 
+        if torch.cuda.is_available():
+            print("cuda is available.")
+            self.cuda()
+
     def forward(self, x):
         """ Forward pass of the neural network. Accepts a tensor of size input_size*input_size. """
-        x = Variable(x.view(-1, self.input_size * self.input_size)).float()
+        x_input = x.view(-1, self.input_size * self.input_size)
+        if torch.cuda.is_available():
+            x_input = x_input.cuda()
+        x = Variable(x_input).float()
         encoded = self.encoder(x)
         return encoded
 
@@ -72,7 +79,11 @@ if __name__ == '__main__':
     for epoch in range(num_epochs):
         for step, (x, y) in enumerate(train_loader):
             # Wrap label 'y' in variable
-            nn_label = Variable(y.view(-1, output_vec))
+            y_label = y.view(-1, output_vec)
+            if torch.cuda.is_available():
+                y_label = y_label.cuda()
+            nn_label = Variable(y_label)
+
             # forward pass
             forward_pass = mkrnn(x)
             loss = loss_func(forward_pass, nn_label)  # compute loss
@@ -85,7 +96,11 @@ if __name__ == '__main__':
                 print('Epoch: ', epoch, 'Step: ', step, '| train loss: %.4f' % loss.data[0])
                 valid_loss = 0
                 for (valid_x, valid_y) in valid_loader:
-                    valid_nn_label = Variable(valid_y.view(-1, output_vec))
+                    valid_y_label = valid_y.view(-1, output_vec)
+                    if torch.cuda.is_available():
+                        valid_y_label = valid_y_label.cuda()
+                    valid_nn_label = Variable(valid_y_label)
+
                     valid_forward_pass = mkrnn(valid_x)
                     valid_loss_eval = loss_func(valid_forward_pass, valid_nn_label)  # compute validation loss
                     valid_loss += valid_loss_eval.data[0]
