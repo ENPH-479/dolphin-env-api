fc0f739b63267dd0e66dc5e161a4ee65c711fd7d
0x4249 <32822229+0x4249@users.noreply.github.com>
2017-12-30 12:39:17 -0800

    Update test_main.py with refactoring.

--- test_main.py
+++ test_main.py
@@ -12,7 +12,7 @@ import torch
 from src import dp_controller, keylog, mk_downsampler, key2pad, helper, dataset_merger
 from src.agents import state_model, mk_naive_agent, mk_nn
 from src.agents.mk_nn_train import MKNN
-from src.agents.mk_rnn_train import MKRNN
+from src.agents.mk_rnn_lstm_train import MKRNN
 from src.agents.mk_cnn_train import MKCNN
 
 # Configure logger
